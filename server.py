56546bab382a00a49e19e507143b31fc72e7f5e7
William Chen <williamchen16@gmail.com>
2015-05-23 22:57:07 -0700

    reindent

--- server.py
+++ server.py
@@ -19,72 +19,77 @@ CLIENT_ID = config.get('amazon', 'client_id')
 CLIENT_SECRET = config.get('amazon', 'client_secret')
 CODE = None
 
+
 @route('/signin')
 def signin_auth():
-	CODE = request.params.get('code')
-	scope = request.params.get('scope')
-	
-	debug = get_login_url() + '\n'
-	debug += 'scope: {}\n'.format(scope)
-	debug += 'code: {}\n'.format(CODE)
-	access_token_request(CODE)
-	print debug
+    CODE = request.params.get('code')
+    scope = request.params.get('scope')
 
-@route('/signin', method = 'POST')
+    debug = get_login_url() + '\n'
+    debug += 'scope: {}\n'.format(scope)
+    debug += 'code: {}\n'.format(CODE)
+    access_token_request(CODE)
+    print debug
+
+
+@route('/signin', method='POST')
 def access_token_response():
-	token_type = request.params.get('token_type')
-	expires_in = request.params.get('expires_in')
-	refresh_token = request.params.get('refresh_token')
-	access_token =  request.params.get('access_token')
+    token_type = request.params.get('token_type')
+    expires_in = request.params.get('expires_in')
+    refresh_token = request.params.get('refresh_token')
+    access_token = request.params.get('access_token')
 
-	print 'refresh_token', refresh_token
-	return refresh_token
+    print 'refresh_token', refresh_token
+    return refresh_token
+
 
 def access_token_request(code):
-	print "Making request"
-	params = {
-		'grant_type': 'authorization_code',
-		'code': code,
-		'client_id': CLIENT_ID,
-		'client_secret': CLIENT_SECRET,
-		'redirect_uri': REDIRECT_URL
-	}
-	headers = {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
-	url = 'https://api.amazon.com/auth/o2/token'
-	try:
-		request = urllib2.Request(url, urlencode(params), headers)
-		print request.get_full_url(), request.get_data(), request.header_items()
-		response = urllib2.urlopen(request)
-		print response.read()
-	except urllib2.HTTPError as e:
-		print e.code, e.reason, e.read()
+    print "Making request"
+    params = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'redirect_uri': REDIRECT_URL
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    url = 'https://api.amazon.com/auth/o2/token'
+    try:
+        request = urllib2.Request(url, urlencode(params), headers)
+        print request.get_full_url(), request.get_data(), request.header_items()
+        response = urllib2.urlopen(request)
+        print response.read()
+    except urllib2.HTTPError as e:
+        print e.code, e.reason, e.read()
 
-	
 
 def get_login_url():
-	params = {
-		'client_id': CLIENT_ID,
-		'scope': 'clouddrive:read clouddrive:write',
-		'response_type': 'code',
-		'redirect_uri': REDIRECT_URL
-	}
-	url = 'https://www.amazon.com/ap/oa/?'
-	return url + urlencode(params)
-	
+    params = {
+        'client_id': CLIENT_ID,
+        'scope': 'clouddrive:read clouddrive:write',
+        'response_type': 'code',
+        'redirect_uri': REDIRECT_URL
+    }
+    url = 'https://www.amazon.com/ap/oa/?'
+    return url + urlencode(params)
+
+
 def get_code():
-	return CODE
+    return CODE
+
 
 def get_config():
-	print config.items('httpd')
-	print config.items('amazon')
+    print config.items('httpd')
+    print config.items('amazon')
+
 
 def start():
-	return;
+    return;
+
 
 if __name__ == '__main__':
-	get_config()
-	print "Visit " + get_login_url()
-	run(host = HOST, port = PORT, debug = True, reloader = True)
-
+    get_config()
+    print "Visit " + get_login_url()
+    run(host=HOST, port=PORT, debug=True, reloader=True)
